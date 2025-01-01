f1f1be59d1f5ece9b2838e2932c546cc49a1e430
Sam <sam@adjeiconsulting.com>
2024-12-31 23:31:51 -0600

    Update deployment configuration for Render

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 3000,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
   }
 }) 
